c483200d6cc32328ac1df9006660a48b7c151fad
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-05-24 03:39:08 +0530

    snooze service using timertask

--- app/src/main/java/com/ujjwal/proxivity/SnoozeReceiver.java
+++ app/src/main/java/com/ujjwal/proxivity/SnoozeReceiver.java
@@ -3,14 +3,34 @@ package com.ujjwal.proxivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.SensorManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SnoozeReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "You snoozed the service.", Toast.LENGTH_SHORT).show();
+    public void onReceive(final Context context, Intent intent) {
+        Toast.makeText(context, "Service snoozed for "+ ScreenOnOffService.seconds + " seconds.", Toast.LENGTH_SHORT).show();
         ScreenOnOffService.sensorManager.unregisterListener(ScreenOnOffService.proximitySensorListener, ScreenOnOffService.proximitySensor);
         ScreenOnOffService.sensorManager.unregisterListener(ScreenOnOffService.accelerometerSensorListener, ScreenOnOffService.accelerometerSensor);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("Proxivity", "Back on track.");
+                ScreenOnOffService.sensorManager.registerListener(ScreenOnOffService.proximitySensorListener, ScreenOnOffService.proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+                ScreenOnOffService.sensorManager.registerListener(ScreenOnOffService.accelerometerSensorListener, ScreenOnOffService.accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        };
+
+        Timer timer = new Timer("ProxivityTimer");
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, ScreenOnOffService.seconds);
+        timer.schedule(timerTask, cal.getTime());
     }
 }
