83beb0493f8d9c6d29935f7c2aabf60f470427f6
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-05-24 14:43:47 +0530

    bug ifx: when screen turns off automatically

--- app/src/main/java/com/ujjwal/proxivity/ScreenOnOffService.java
+++ app/src/main/java/com/ujjwal/proxivity/ScreenOnOffService.java
@@ -110,7 +110,7 @@ public class ScreenOnOffService extends Service {
                         Log.i(TAG, "Not an admin");
                     }
                 }
-                else if (event.values[1] < -2.8 && pflag == 0 && Build.VERSION.SDK_INT > 19 && display.getState() == Display.STATE_OFF) {
+                else if (event.values[1] < -2.8 && pflag == 0 && Build.VERSION.SDK_INT > 19 && display.getState() != Display.STATE_ON) {
                     wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "DEBUG: My Tag");
                     wl.acquire(1);
                     if (wl.isHeld()) wl.release();
